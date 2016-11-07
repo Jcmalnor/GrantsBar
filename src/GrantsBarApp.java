d1e02a55d51d0847110c9e71fc00179b55bbb244
Jeff Malnor <jcmalnor@gmail.com>
2016-11-07 15:59:35 -0500

    first commit

---
+++ src/GrantsBarApp.java
@@ -0,0 +1,17 @@
+
+public class GrantsBarApp {
+
+	public static void main(String[] args) {
+		
+		//introduction
+		//call menu
+		//call take order
+		//calculate total
+		//call checkout
+			//payment method
+		//print receipt
+		//Again?
+		
+	}
+
+}
