1a996ec49e54b5721bb4c678a7ac0ca84e5e5c78
Milliza Malabanan <milliza.mae@gmail.com>
2016-11-08 15:18:31 -0500

    deleted grant's bar sub folder

--- src/GrantsBarApp.java
+++ src/GrantsBarApp.java
@@ -3,10 +3,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 public class GrantsBarApp {
 
 	public static void main(String[] args) {
+		
 		// introduction
 		System.out.println("Hello and welcome to Grant's Bar!\n");
 		// read and print menu
@@ -15,6 +18,7 @@ public class GrantsBarApp {
 		TakeOrder order = new TakeOrder();
 		double total1 = 0;
 		double total2 = 0;
+		String menu = "";
 		
 		try {
 			FileReader fr = new FileReader(file);
@@ -30,41 +34,59 @@ public class GrantsBarApp {
 			System.out.println(e);
 		}
 		
-<<<<<<< HEAD
-		System.out.println("hello");
-=======
-		while (newOrder.equalsIgnoreCase("n")) {
-			
-			// call take order
-			newOrder = order.getTakeOrder();
-			// calculate total
-			total2 = order.getTotal();
-	
-			if (newOrder.equalsIgnoreCase("y")) {
-				total1 += total2;
-				break;
-			} else {
-				continue;
-			}	
-			}
-			total1 = order.getTax();
-			System.out.println("Your total after tax is $" + total1);
-			
-			total1 = order.getTip();
-			System.out.println(total1);
-			
-			// call checkout
-			// payment method
+		Scanner file = new Scanner (new File("Menu")).useDelimiter(",\\"); 
+		List <String> menu = new ArrayList<String>();
+		 while (file.hasNext()) {
+		      // find next line
+		      menu = inFile1.next();
+		      temps.add(menu);
+		    }
+		    file.close();
+		
+		    String[] menuArray = menu.toArray(new String[5]);
 
-			/*
-			 * print receipt drink number price payment change
-			 */
-
-			// New Order?
-			// if yes loop
-			// if no thanks for coming
+		    for (String m : menuArray) {
+		      System.out.println(m);
+		    }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		while (newOrder.equalsIgnoreCase("n")) {
+//			
+//			// call take order
+//			newOrder = order.getTakeOrder();
+//			// calculate total
+//			total2 = order.getTotal();
+//	
+//			if (newOrder.equalsIgnoreCase("y")) {
+//				total1 += total2;
+//				break;
+//			} else {
+//				continue;
+//			}	
+//			}
+//			total1 = order.getTax();
+//			System.out.println("Your total after tax is $" + total1);
+//			
+//			total1 = order.getTip();
+//			System.out.println(total1);
+//			
+//			// call checkout
+//			// payment method
+//
+//			/*
+//			 * print receipt drink number price payment change
+//			 */
+//
+//			// New Order?
+//			// if yes loop
+//			// if no thanks for coming
 
 		}
->>>>>>> ca849bc10e7938efac679794bb006117092e4458
-	}
-
+}
